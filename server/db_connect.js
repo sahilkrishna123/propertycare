a84ba46d3236b77795e15e9d4fbb4f87398471c0
SAHIL KRISHNA <sahilkrishna1243@gmail.com>
2024-09-27 01:23:10 +0500

    Initial MongoDB Setup

--- server/db_connect.js
+++ server/db_connect.js
@@ -1,40 +1,40 @@
-import  mysql from 'mysql2';
-import fs from 'fs';
+import mysql from "mysql2";
+import fs from "fs";
 
 // Create a connection to the remote database
 
 // Read the SQL file
-const sqlFile = fs.readFileSync('./stateagency.sql', 'utf8');
+const sqlFile = fs.readFileSync("./stateagency.sql", "utf8");
 
 // Connect to the database
 connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to the database:', err);
+    console.error("Error connecting to the database:", err);
     return;
   }
-  console.log('Connected to the remote MySQL database.');
+  console.log("Connected to the remote MySQL database.");
 
   // Example query
-//   connection.query('SELECT * FROM your_table', (err, results) => {
-  connection.query('show tables', (err, results) => {
+  //   connection.query('SELECT * FROM your_table', (err, results) => {
+  connection.query("show tables", (err, results) => {
     if (err) {
-      console.error('Error executing query:', err);
+      console.error("Error executing query:", err);
       return;
     }
-    console.log('Query results:', results);
-    
+    console.log("Query results:", results);
+
     // Close the connection
     connection.end();
   });
 
-//    // Run the SQL file
-//    connection.query(sqlFile, (err, results) => {
-//     if (err) {
-//       console.error('Error running SQL script:', err);
-//       return;
-//     }
+  //    // Run the SQL file
+  //    connection.query(sqlFile, (err, results) => {
+  //     if (err) {
+  //       console.error('Error running SQL script:', err);
+  //       return;
+  //     }
 
-//     console.log('SQL script executed successfully:', results);
-//     connection.end(); // Close the connection when done
-//   });
+  //     console.log('SQL script executed successfully:', results);
+  //     connection.end(); // Close the connection when done
+  //   });
 });
